decdfd5bab7dbba3de0105285997f1c1e87f321d
Michu0112 <66910540+Michu0112@users.noreply.github.com>
2021-01-22 00:42:13 +0100

    add animations

---
+++ scripts/animation.js
@@ -0,0 +1,16 @@
+const mobileLogo = document.getElementById('mobile-logo');
+const animParagraph = document.querySelector('.anim-paragraph');
+const animSubParagraph = document.querySelector('.anim-sub-paragraph')
+const navBtn = document.getElementById('nav-btn');
+
+mobileLogo.style.opacity = "1";
+mobileLogo.style.transform = 'translateY(0)';
+
+animParagraph.style.opacity = "1";
+animParagraph.style.transform = 'translateX(0)';
+
+animSubParagraph.style.opacity = "1";
+animSubParagraph.style.transform = 'translateY(0)'
+
+navBtn.style.opacity = "1";
+navBtn.style.transform = "translateY(0px)";
